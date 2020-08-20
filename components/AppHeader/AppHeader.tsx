08935b1933fc89e65156ef0274e04895c9c571a2
kimulaco <kmrkdev@gmail.com>
2020-08-21 08:45:12 +0900

    feat: add AppHeader style

--- components/AppHeader/AppHeader.tsx
+++ components/AppHeader/AppHeader.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from './AppHeader.module.scss'
 
 const AppHeader: React.FC = () => {
   return (
-    <header>
-      <div>
-        <h1><Link href="/"><a>sass-diff</a></Link></h1>
+    <header className={styles.AppHeader}>
+      <div className={styles.AppHeader_inner}>
+        <h1 className={styles.AppHeader_title}>
+          <Link href="/"><a>sass-diff</a></Link>
+        </h1>
       </div>
     </header>
   )
